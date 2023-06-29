3c22fbc005fc8ec12497eba77ebba8e77d66c178
bcj <brendan@bcjbcj.ca>
2023-06-29 13:43:29 -0500

    Start test server with locations in place

--- tests/server.py
+++ tests/server.py
@@ -1,8 +1,10 @@
 """Run an interactive test server using a temporary configuration"""
 import asyncio
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from picpocket.web import DEFAULT_PORT, run_server
-from tests.conftest import _load_api
+from tests.conftest import IMAGE_FILES, _load_api
 
 
 def main():
@@ -12,10 +14,16 @@ def main():
 
 async def temporary_server(port: int):
     async with _load_api() as api:
-        try:
-            await run_server(api, port)
-        except KeyboardInterrupt:
-            pass
+        with TemporaryDirectory() as dirname:
+            directory = Path(dirname)
+
+            await api.add_location("camera", source=True, removable=True)
+            main_id = await api.add_location("main", directory, destination=True)
+
+            try:
+                await run_server(api, port)
+            except KeyboardInterrupt:
+                pass
 
 
 if __name__ == "__main__":
